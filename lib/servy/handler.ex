b9a2d00615edddff3a4c082abd435b00604608b9
rmap96 <rmap96@hotmail.com>
2018-05-21 01:10:55 -0500

    Added new verification for html file

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -55,7 +55,14 @@ defmodule Servy.Handler do
   end
 
   def route(%{method: "GET", path: "/about"} = conv) do
-    %{ conv | status: 200, resp_body: "contents of file"}
+    case File.read("/home/rocio/servy/lib/pages/about.html") do
+      {:ok, content} ->
+        %{ conv | status: 200, resp_body: content}
+      {:error, :enoent} ->
+        %{ conv | status: 404, resp_body: "FIle not found"}
+      {:error, reason} ->
+        %{ conv | status: 500, resp_body: "File error: #{reason}" }
+    end
   end
 
   def route(%{ path: path } = conv) do
