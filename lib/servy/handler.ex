8f9ca14050455dfef9c4855002cbea69a90af717
rmap96 <rmap96@hotmail.com>
2018-05-22 09:18:02 -0500

    Added structures to conv.ex

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,6 +1,8 @@
 defmodule Servy.Handler do
-  
+
   @moduledoc "Handles HTTP requests"
+
+  alias Servy.Conv
 
   @pages_path Path.expand("../../pages", __DIR__)
   
@@ -18,30 +20,26 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  #def route(conv) do
-  #  route(conv, conv.path, conv.path)
-  #end
-
-  def route(%{method: "GET", path: "/wildthings"} = conv) do
+  def route(%Conv{method: "GET", path: "/wildthings"} = conv) do
     %{ conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
-  def route(%{method: "GET", path: "/bears"} = conv) do
+  def route(%Conv{method: "GET", path: "/bears"} = conv) do
     %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
   end
 
-  def route(%{method: "GET", path: "/bears/" <> id } = conv) do
+  def route(%Conv{method: "GET", path: "/bears/" <> id } = conv) do
     %{ conv | status: 200, resp_body: "Bear #{id}" }
   end
 
-  def route(%{method: "GET", path: "/about"} = conv) do
+  def route(%Conv{method: "GET", path: "/about"} = conv) do
       @pages_path
       |> Path.join("about.html")
       |> File.read
       |> handle_file(conv)
   end
 
-  def route(%{ path: path } = conv) do
+  def route(%Conv{ path: path } = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here!"}
   end 
 
@@ -57,42 +55,15 @@ defmodule Servy.Handler do
     %{ conv | status: 500, resp_body: "File error: #{reason}" }
   end
 
-  #def route(%{method: "GET", path: "/about"} = conv) do
-  #  file =
-  #    Path.expand("../../pages", __DIR__)
-  #    |> Path.join("about.html")
-  #  case File.read(file) do
-  #    {:ok, content} ->
-  #      %{ conv | status: 200, resp_body: content}
-  #    {:error, :enoent} ->
-  #      %{ conv | status: 404, resp_body: "FIle not found"}
-  #    {:error, reason} ->
-  #      %{ conv | status: 500, resp_body: "File error: #{reason}" }
-  #  end
-  #end
-
-  def format_response(conv) do
-    #TODO: Use values in the map to create an HTTP response string
+  def format_response(%Conv{} = conv) do
     """
-    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    HTTP/1.1 #{Conv.full_status(conv)}
     Content-Type: text/html
     Content-Lenght: #{String.length(conv.resp_body)}
 
     #{conv.resp_body}
     """
   end
-  
-  defp status_reason(code) do
-    %{
-      200 => "OK",
-      201 => "Created",
-      401 => "Unauthorized",
-      403 => "Forbidden",
-      404 => "Not Found",
-      500 => "Iterna Server Error",
-    }[code]
-  end
-
 
 end
 
