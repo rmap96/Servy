a391d5766646d4160fe14d0040c9c4e037f50142
rmap96 <rmap96@hotmail.com>
2018-05-24 22:37:02 -0500

    comprehensions

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -4,21 +4,21 @@ defmodule Servy.Parser do
 
   def parse(request) do
     [top, params_string] = String.split(request, "\n\n")
-      
+
     [request_line | header_lines] = String.split(top, "\n")
-      
-    [method,  path, _] = String.split(request_line, " ")
+
+    [method, path, _] = String.split(request_line, " ")
 
     headers = parse_headers(header_lines, %{})
-      
+
     params = parse_params(headers["Content-Type"], params_string)
-      
+
     %Conv{ 
-      method: method,
-      path: path,
-      params: params,
-      headers: headers
-    }
+       method: method, 
+       path: path,
+       params: params,
+       headers: headers
+     }
   end
 
   def parse_headers([head | tail], headers) do
@@ -28,7 +28,7 @@ defmodule Servy.Parser do
   end
 
   def parse_headers([], headers), do: headers
-  
+
   def parse_params("application/x-www-form-urlencoded", params_string) do
     params_string |> String.trim |> URI.decode_query
   end
@@ -36,3 +36,5 @@ defmodule Servy.Parser do
   def parse_params(_, _), do: %{}
 
 end
+
+
